dbcff439cbad0e7be5e78b4cd07fbc997e3893e1
Pranshu <pranshumittal10@gmail.com>
2024-01-12 00:03:29 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-// // App.js
-// import React from 'react';
-// import BulletinBoard from './BulletinBoard';
-// import './styles.css';
-
-// function App() {
-//   return (
-//     <div>
-//       <BulletinBoard />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// src/App.js
 import React from 'react';
 import Board from './Board';
 
